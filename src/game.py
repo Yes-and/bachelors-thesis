95c0123da1de8ca6fa26764f143d24cb1d868648
yes-and <vojtawilson@gmail.com>
2025-02-11 23:06:01 +0100

    Remove usage of environmental variables, streamline logic

--- src/game.py
+++ src/game.py
@@ -21,96 +21,75 @@ class CustomGame(Game):
         self,
         players: list[Player],
         expansions: list[list[Card]],
-        exp_buffer: ExperienceBuffer,
-        kingdom_cards: list[Card]|None = None,
-        start_deck: list[Card]|None = None,
-        random_order: bool = True,
-        log_stdout: bool = True,
-        log_file: bool = False,
-        log_file_name: str = "game.log",
+        exp_buffer: ExperienceBuffer
     ):
         super().__init__(
-            players,
-            expansions,
-            kingdom_cards,
-            start_deck,
-            random_order,
-            log_stdout,
-            log_file,
-            log_file_name
+            players=players,
+            expansions=expansions
         )
         self.exp_buffer = exp_buffer
         self.total_rewards = []
-        self.game_rewards = []
+        self.victory_points = 0
+        self.last_state = None
+        self.last_action = None
+        self.max_turns = 50
 
     def play(self) -> GameResult:
-        last_experience = None
         self.start()
         while True:
             for player in self.players:
                 self.current_player = player
                 self.play_turn(player)
+                # TODO: add proper logging
 
-                if self.current_player.player_id == 'my_bot':
-                    # State after action
-                    game_state = {str(pile.name): len(pile) for pile in self.supply.piles}
-                    player_cards = [str(card) for card in self.current_player.get_all_cards()]
-                    player_state = {card: 0 for card in game_state.keys()}
-                    for card in player_cards:
-                        player_state[card] += 1
+                if player.turns >= self.max_turns:
+                    result = self.summarize_game()
+                    logging.info(f"Game was stopped after {self.max_turns} turns, \n{result}")
+                    return result
 
-                    state = json.loads(os.environ['STATE'])
-                    action = int(os.environ['ACTION'])
-                    reward = float(os.environ['REWARD'])
-                    self.total_rewards.append(reward)
+                if player.player_id == 'my_bot':
+                    state = player.state_before_action
+                    action = player.decider.last_action
+                    reward = 0
                     done = 0
-                    new_state = None
+                    new_state = player.state_after_action
+
+                    self.last_state = new_state
+                    self.last_action = action
+
+                    self.total_rewards.append(reward)
+
                     current_experience = Experience(
                         state, action, reward, done, new_state
                     )
-                    if not last_experience:
-                        last_experience = current_experience
-                    else:
-                        last_experience = Experience(
-                            last_experience[0],
-                            last_experience[1],
-                            last_experience[2],
-                            last_experience[3],
-                            current_experience[0]
-                        )
-                        self.exp_buffer.append(last_experience)
+                    self.exp_buffer.append(current_experience)
 
                 if self.is_over():
-                    money = self.current_player.state.money
-                    buys = self.current_player.state.buys
-
-                    # Game state
-                    pile_cards = {str(pile.name): len(pile) for pile in self.supply.piles}
-
-                    # Player state
-                    player_cards = {name: 0 for name in pile_cards.keys()}
-                    for card in self.current_player.get_all_cards():
-                        player_cards[str(card)] += 1
-
+                    state = self.last_state
+                    action = self.last_action
+                    # Get reward
                     winners = self.get_winners()
                     if ('my_bot' in [str(winner) for winner in winners]) \
                         and (len(winners)==1):
                         reward = 1.0
                     else:
                         reward = -1.0
-                    self.total_rewards.append(reward)
-                    self.game_rewards.append(reward)
-
                     done = 1
-
-                    complete_state = list(pile_cards.values()) + \
-                        list(player_cards.values()) + \
-                        [money] + [buys]
-                    
-                    finishing_experience = Experience(
-                        current_experience[0], current_experience[1], reward, done, complete_state
+                    # Get new state
+                    i = [str(player) for player in self.players].index('my_bot')
+                    self.players[i].decider.set_current_state(
+                        player=self.players[i],
+                        game=self
                     )
-                    self.exp_buffer.append(finishing_experience)
+                    new_state = self.players[i].decider.last_state
+
+                    self.total_rewards.append(reward)
+                    self.victory_points = self.players[i].get_victory_points()
+
+                    current_experience = Experience(
+                        state, action, reward, done, new_state
+                    )
+                    self.exp_buffer.append(current_experience)
 
                     result = self.summarize_game()
                     logging.info(f"\n{result}")
