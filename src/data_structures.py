2893a3684f88888f6aad651a4be231d0bdba09a9
yes-and <vojtawilson@gmail.com>
2025-03-09 18:05:06 +0100

    Add data structures for A2C

--- src/data_structures.py
+++ src/data_structures.py
@@ -1,31 +1,37 @@
-import collections
-import numpy as np
+from multiprocessing import Manager
 
+class ExperienceMemory:
+    def __init__(self):
+        manager = Manager()
+        self.states = manager.list()
+        self.actions = manager.list()
+        self.log_probs = manager.list()
+        self.rewards = manager.list()
+        self.dones = manager.list()
 
-Experience = collections.namedtuple(
-    'Experience', 
-    field_names=[
-        'state', 'action', 'reward', 'done', 'new_state'
-    ]
-)
+    def store(self, state, action, log_prob, reward, done):
+        """Store a single transition."""
+        self.states.append(state)
+        self.actions.append(action)
+        self.log_probs.append(log_prob)
+        self.rewards.append(reward)
+        self.dones.append(done)
 
-# class ExperienceBuffer:
-#     def __init__(self, capacity):
-#         self.buffer = collections.deque(maxlen=capacity)
+    def store_batch(self, new_experiences):
+        """Store an entire batch of experiences at once to reduce locking overhead."""
+        self.states.extend(new_experiences.states)
+        self.actions.extend(new_experiences.actions)
+        self.log_probs.extend(new_experiences.log_probs)
+        self.rewards.extend(new_experiences.rewards)
+        self.dones.extend(new_experiences.dones)
 
-#     def __len__(self):
-#         return len(self.buffer)
+    def clear(self):
+        """Clear all stored experiences."""
+        self.states[:] = []
+        self.actions[:] = []
+        self.log_probs[:] = []
+        self.rewards[:] = []
+        self.dones[:] = []
 
-#     def append(self, experience):
-#         self.buffer.append(experience)
-
-#     def sample(self, batch_size):
-#         indices = np.random.choice(
-#             len(self.buffer), batch_size, replace=False
-#         )
-#         states, actions, rewards, dones, next_states = \
-#             zip(*[self.buffer[idx] for idx in indices])
-#         return np.array(states), np.array(actions), \
-#                np.array(rewards, dtype=np.float32), \
-#                np.array(dones, dtype=np.uint8), \
-#                np.array(next_states)
+    def __len__(self):
+        return len(self.states)
