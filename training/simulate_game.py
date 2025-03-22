15e4cb753ee9201f041bca4afe3bb495f3b0b900
yes-and <vojtawilson@gmail.com>
2025-03-22 20:20:49 +0100

    Change game simulation to REINFORCE

--- training/simulate_game.py
+++ training/simulate_game.py
@@ -1,50 +1,44 @@
-import numpy as np
-import torch
-from pyminion.bots.examples import BigMoney
-
-from src.actor_critic_model import ActorCritic
+from src.network_model import PolicyNetwork
+from game_logic.data_structures import GameExperienceMemory
 from game_logic.game import CustomGame
-from game_logic.sets import custom_set
 from game_logic.bots.my_bot import MyBot
+from game_logic.sets import custom_set
+
+from pyminion.bots.examples import BigMoney
+import torch
 
 
 
 def play_game(g):
-    """Runs a game using a locally loaded PPO policy network and returns an experience buffer."""
-    # Load policy net inside the subprocess
-    policy_net = ActorCritic(g.STATE_SIZE, g.ACTION_SIZE).to(g.DEVICE)
+    policy_net = PolicyNetwork(g.STATE_SIZE, g.ACTION_SIZE).to(g.DEVICE)
     policy_net.load_state_dict(torch.load(g.MODEL_PATH, map_location=g.DEVICE)) # Load the network
     policy_net.eval() # Sets network to evaluation mode
 
+    memory = GameExperienceMemory()
+
     # Set up the game
     bot1 = BigMoney()
-    bot2 = MyBot(net=policy_net)
+    bot2 = MyBot(net=policy_net, prob_action=1/25, memory=memory)
     game = CustomGame(
         players=[bot1, bot2],
         expansions=[custom_set],
         log_stdout=False,
+        memory=memory
     )
     game.play()  # Fills game.exp_buffer
 
-    exp = game.exp_buffer
-
-    # Generalized advantage estimation
-    advantages = np.zeros(len(exp.rewards))  # Initialize advantage array
-    last_advantage = 0
-
-    for i in reversed(range(len(exp.rewards))):
-        # Compute TD error (delta)
-        delta = exp.rewards[i] + g.GAMMA * (exp.state_values[i + 1] if i < len(exp.rewards) - 1 else 0) - exp.state_values[i]
-        
-        # Compute GAE advantage
-        advantages[i] = last_advantage = delta + g.GAMMA * g.LAMBDA * last_advantage
-
-    # Normalize advantages and add them to the buffer
-    exp.advantages = advantages
-
-    # Extract information regarding the number of victory points
-    player_vp = game.player_vp
-    enemy_vp = game.enemy_vp
-    turns = game.player_turns
-
-    return exp, player_vp, enemy_vp, turns
+    if (
+        (memory.state is None) or \
+        (memory.action is None) or \
+        (memory.turn is None) or \
+        (memory.end_turns is None) or \
+        (memory.end_reward is None)
+    ):
+        return None, None, None
+        # memory.action = 18
+        # memory.turn = 1
+        # memory.state = np.array( # Starting state
+        #     [1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,0.375,0.,0.,0.,0.15217391,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.]
+        # )
+    else:
+        return memory, game.player_vp, game.enemy_vp
