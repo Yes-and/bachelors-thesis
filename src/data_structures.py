14ce68342dc5aa08d61ef9e048e9e290b12ce25a
yes-and <vojtawilson@gmail.com>
2025-03-22 20:20:25 +0100

    Update main data structure

--- src/data_structures.py
+++ src/data_structures.py
@@ -5,42 +5,34 @@ class MultiprocessingExperienceMemory:
         manager = Manager()
         self.states = manager.list()
         self.actions = manager.list()
-        self.rewards = manager.list()
-        self.log_probs = manager.list()
-        self.advantages = manager.list()
-        self.state_values = manager.list()
-        self.dones = manager.list()
+        self.turns = manager.list()
+        self.end_turns = manager.list()
+        self.end_rewards = manager.list()
 
-    def store_batch(self, new_experiences):
+    def store_batch(self, new_experience):
         """Store an entire batch of experiences at once to reduce locking overhead."""
-        self.states.extend(new_experiences.states)
-        self.actions.extend(new_experiences.actions)
-        self.rewards.extend(new_experiences.rewards)
-        self.log_probs.extend(new_experiences.log_probs)
-        self.advantages.extend(new_experiences.advantages)
-        self.state_values.extend(new_experiences.state_values)
-        self.dones.extend(new_experiences.dones)
+        self.states.append(new_experience.state)
+        self.actions.append(new_experience.action)
+        self.turns.append(new_experience.turn)
+        self.end_turns.append(new_experience.end_turns)
+        self.end_rewards.append(new_experience.end_reward)
 
     def clear(self):
         """Clear all stored experiences."""
         self.states[:] = []
         self.actions[:] = []
-        self.rewards[:] = []
-        self.log_probs[:] = []
-        self.advantages[:] = []
-        self.state_values[:] = []
-        self.dones[:] = []
+        self.turns[:] = []
+        self.end_turns[:] = []
+        self.end_rewards[:] = []
 
     def get_batch(self):
         """Return normal lists before training."""
         return (
             list(self.states),
             list(self.actions),
-            list(self.rewards),
-            list(self.log_probs),
-            list(self.advantages),
-            list(self.state_values),
-            list(self.dones)
+            list(self.turns),
+            list(self.end_turns),
+            list(self.end_rewards)
         )
 
     def __len__(self):
