5aca11542328d337f65d69c662ca7bbf6ebd77f8
yes-and <vojtawilson@gmail.com>
2025-03-22 20:20:34 +0100

    Adapt testing file

--- test_network.py
+++ test_network.py
@@ -1,6 +1,7 @@
 import copy
 
-from src.actor_critic_model import ActorCritic
+from src.network_model import PolicyNetwork
+from game_logic.data_structures import GameExperienceMemory
 from game_logic.game import CustomGame
 from game_logic.bots.my_bot import MyBot
 from game_logic.sets import custom_set
@@ -16,57 +17,31 @@ class GlobalVariables:
     STATE_SIZE = 34
     ACTION_SIZE = 18
 
-    # Hyperparameters
-    GAMMA = 0.99  # Discount factor
-    LAMBDA = 0.95 # Bias-variance tradeoff factor
-    LR = 1e-5  # Learning rate
-    EPSILON_CLIP = 0.2  # PPO Clipping range
-    EPOCHS = 10  # Number of tratates, actions, loning epochs per batch
-    BATCH_SIZE = 64  # Mini-batch size for training
-    EPISODES = 5000  # Training episodes
-
-    # Others
-    NUM_GAMES_PARALLEL = 4
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    MODEL_PATH = "./models/temp-policy-net.pth"
-
-    # Logging
-    SAVE_MODEL_FREQUENCY = 200
-    LOG_FREQUENCY = 100
+    MODEL_PATH = "./models/model-2025-03-22 20-11-50.pth"
 
 g = GlobalVariables()
 
-policy_net = ActorCritic(g.STATE_SIZE, g.ACTION_SIZE).to(g.DEVICE)
-# policy_net.load_state_dict(torch.load(g.MODEL_PATH, map_location=g.DEVICE)) # Load the network
+policy_net = PolicyNetwork(g.STATE_SIZE, g.ACTION_SIZE).to(g.DEVICE)
+policy_net.load_state_dict(torch.load(g.MODEL_PATH, map_location=g.DEVICE)) # Load the network
 policy_net.eval() # Sets network to evaluation mode
+
+memory = GameExperienceMemory()
 
 # Set up the game
 bot1 = BigMoney()
-bot2 = MyBot(net=policy_net)
+bot2 = MyBot(net=policy_net, prob_action=1/25, memory=memory)
 game = CustomGame(
     players=[bot1, bot2],
     expansions=[custom_set],
-    log_stdout=False,
+    log_stdout=True,
+    memory=memory
 )
 game.play()  # Fills game.exp_buffer
 
-exp = game.exp_buffer
-
-# Generalized advantage estimation
-advantages = np.zeros(len(exp.rewards))  # Initialize advantage array
-last_advantage = 0
-
-for i in reversed(range(len(exp.rewards))):
-    # Compute TD error (delta)
-    delta = exp.rewards[i] + g.GAMMA * (exp.state_values[i + 1] if i < len(exp.rewards) - 1 else 0) - exp.state_values[i]
-    
-    # Compute GAE advantage
-    advantages[i] = last_advantage = delta + g.GAMMA * g.LAMBDA * last_advantage
-
-# Normalize advantages and add them to the buffer
-exp.advantages = advantages
-
-# Extract information regarding the number of victory points
-player_vp = game.player_vp
-enemy_vp = game.enemy_vp
-turns = game.player_turns
+if memory.action is None:
+    memory.action = 18
+    memory.turn = 1
+    memory.state = np.array( # Starting state
+        [1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,0.375,0.,0.,0.,0.15217391,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.]
+    )
