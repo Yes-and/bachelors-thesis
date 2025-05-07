1c5d6a39bc5a96b2df5606340d7982d9873ab9f5
yes-and <vojtawilson@gmail.com>
2025-05-07 13:25:27 +0200

    Update and rename training files for the REINFORCE agent

--- train_REINFORCE.py
+++ train_REINFORCE.py
@@ -4,12 +4,12 @@ import multiprocessing
 import os
 import shutil
 
-from src.data_structures import MultiprocessingExperienceMemory
+from src.buffers import MultiprocessingExperienceMemory
 from src.logger import setup_logger
-from src.network_model import PolicyNetwork
+from src.networks import REINFORCE
 
-from training.simulate_game import play_game
-from training.train_network import train_policy
+from training.simulate_game import REINFORCE_play_game
+from training.train_network import REINFORCE_train
 
 from tensorboardX import SummaryWriter
 import torch
@@ -24,17 +24,16 @@ class GlobalVariables:
 
     # Hyperparameters
     LR = 1e-3  # Learning rate
-    N_GAMES_TRAINING = 128 # Mini-batches for more efficient learning
+    N_GAMES_TRAINING = 128 # Essentially the batch size
     EPISODES = 100000  # Training episodes
 
     # Others
     NUM_GAMES_PARALLEL = 4
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    MODEL_PATH = "./models/temp-policy-net.pth"
+    MODEL_PATH = "./models/REINFORCE-policy-net.pth"
 
     # Logging
     SAVE_MODEL_FREQUENCY = 5000
-    LOG_FREQUENCY = 100
 
 
 
@@ -48,7 +47,7 @@ if __name__ == "__main__":
         print(f"Deleted existing file: {g.MODEL_PATH}")
     
     # Create random model, save it and delete from memory
-    policy_net = PolicyNetwork(g.STATE_SIZE, g.ACTION_SIZE).to(g.DEVICE)
+    policy_net = REINFORCE(g.STATE_SIZE, g.ACTION_SIZE).to(g.DEVICE)
     torch.save(policy_net.state_dict(), g.MODEL_PATH)
     optimizer = optim.Adam(policy_net.parameters(), lr=g.LR)
 
@@ -57,8 +56,9 @@ if __name__ == "__main__":
     writer = SummaryWriter()
 
     # Log hyperparameters
-    logger.info("Training REINFORCE while playing against BigMoney")
+    logger.info("Training a REINFORCE agent.")
     logger.info(f"LR: {g.LR}")
+    logger.info(f"N_GAMES_TRAINING: {g.N_GAMES_TRAINING}")
     logger.info(f"NUM GAMES PARALLEL: {g.NUM_GAMES_PARALLEL}")
 
     # Shared experience memory optimized for multiprocessing
@@ -67,11 +67,11 @@ if __name__ == "__main__":
 
     # Parallel Execution
     with concurrent.futures.ProcessPoolExecutor(max_workers=g.NUM_GAMES_PARALLEL) as executor:
-        next_log, next_train, next_save_model = g.LOG_FREQUENCY, g.N_GAMES_TRAINING, g.SAVE_MODEL_FREQUENCY
+        next_train, next_save_model = g.N_GAMES_TRAINING, g.SAVE_MODEL_FREQUENCY
 
         while True:
             # Start multiple games in parallel
-            futures = [executor.submit(play_game, g) for _ in range(g.NUM_GAMES_PARALLEL)]
+            futures = [executor.submit(REINFORCE_play_game, g) for _ in range(g.NUM_GAMES_PARALLEL)]
 
             # Wait for all workers to complete before continuing
             completed, _ = concurrent.futures.wait(futures)
@@ -84,8 +84,8 @@ if __name__ == "__main__":
                         writer.add_scalar("victory_points_player", player_vp, game_counter)
                         writer.add_scalar("victory_points_enemy", enemy_vp, game_counter)
                         writer.add_scalar("player_turns", new_experience.end_turns, game_counter)
-                        writer.add_scalar("player_reward", new_experience.end_reward, game_counter)
-                        logger.info(f"Game {game_counter} completed. Player VP: {player_vp}, Enemy VP: {enemy_vp}")
+                        writer.add_scalar("player_reward", new_experience.reward, game_counter)
+                        logger.info(f"Game {game_counter} completed.")
                         game_counter += 1
                     else:
                         logger.warning("Worker returned no experiences.")
@@ -94,37 +94,30 @@ if __name__ == "__main__":
                 except Exception as e:
                     logger.error(f"Exception in worker: {str(e)}")
 
-            # Log at regular intervals
-            if game_counter >= next_log:
-                logger.info(f"{game_counter} games have been played.")
-                next_log += g.LOG_FREQUENCY
-
             # Train after collecting a full batch
             if game_counter >= next_train:
                 logger.info(f"Collected {len(safe_exp_buffer)} experiences. Training now...")
-                states, actions, turns, end_turns, end_rewards = safe_exp_buffer.get_batch()
-                loss, entropy = train_policy(
+                states, actions, rewards, _, _ = safe_exp_buffer.get_batch()
+                loss, entropy = REINFORCE_train(
                     g=g,
                     policy_net=policy_net,
                     optimizer=optimizer,
                     states=states,
                     actions=actions,
-                    turns=turns,
-                    end_turns=end_turns,
-                    end_rewards=end_rewards
+                    rewards=rewards
                 )
                 writer.add_scalar("loss", loss, game_counter)
                 writer.add_scalar("entropy", entropy, game_counter)
-                logger.info("Policy network has been fitted. Clearing the buffer...")
+                logger.info("Policy network for REINFORCE has been fitted. Clearing the buffer...")
                 safe_exp_buffer.clear()
                 next_train += g.N_GAMES_TRAINING
 
             # Save model periodically
             if game_counter >= next_save_model:
-                logger.info("Saving policy network model.")
+                logger.info("Saving policy network model for REINFORCE.")
                 timestamp = str(datetime.datetime.now())[:19].replace(":", "-")
-                shutil.copy("./models/temp-policy-net.pth", f"./models/model-{timestamp}.pth")
-                logger.info("Policy network has been saved.")
+                shutil.copy("./models/REINFORCE-policy-net.pth", f"./models/REINFORCE-model-{timestamp}.pth")
+                logger.info("Policy network for REINFORCE agent has been saved.")
                 next_save_model += g.SAVE_MODEL_FREQUENCY
 
             if game_counter >= g.EPISODES:
