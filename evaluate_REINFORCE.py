d3ae8f32442a25ae34902448e21307a333c93fa6
Yes-and <72066894+Yes-and@users.noreply.github.com>
2025-05-11 10:37:49 +0200

    Create evaluate_REINFORCE.py
    Add final for manual evaluation of choices within the game

---
+++ evaluate_REINFORCE.py
@@ -0,0 +1,43 @@
+import copy
+
+from src.networks import REINFORCE
+from game_logic.data_structures import GameExperienceMemory
+from game_logic.game import CustomGame
+from game_logic.bots.my_bot import MyBot
+from game_logic.sets import custom_set
+
+import numpy as np
+from pyminion.bots.examples import BigMoney
+from pyminion.simulator import Simulator
+import torch
+
+
+
+class GlobalVariables:
+    # State representation
+    STATE_SIZE = 34
+    ACTION_SIZE = 18
+
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    MODEL_PATH = "./selected_models/REINFORCE-model.pth"
+
+g = GlobalVariables()
+
+policy_net = REINFORCE(g.STATE_SIZE, g.ACTION_SIZE).to(g.DEVICE)
+policy_net.load_state_dict(torch.load(g.MODEL_PATH, map_location=g.DEVICE)) # Load the network
+policy_net.eval() # Sets network to evaluation mode
+
+memory = GameExperienceMemory()
+
+# Set up the game
+bot1 = BigMoney()
+bot2 = MyBot(net=policy_net, memory=memory, g=g)
+game = CustomGame(
+    players=[bot1, bot2],
+    expansions=[custom_set],
+    log_stdout=True,
+    memory=memory
+)
+
+result = game.play()
+print(result)
