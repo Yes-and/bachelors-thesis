583e7f4e9e8e54c42b993d3d62f87d3eb4945aaa
Yes-and <72066894+Yes-and@users.noreply.github.com>
2025-05-11 10:38:58 +0200

    Change test REINFORCE
    Change testing file for REINFORCE, add model and save arena results

--- test_REINFORCE.py
+++ test_REINFORCE.py
@@ -19,7 +19,7 @@ class GlobalVariables:
     ACTION_SIZE = 18
 
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    MODEL_PATH = "./models/REINFORCE-model-2025-05-07 14-39-13.pth"
+    MODEL_PATH = "./selected_models/REINFORCE-model.pth"
 
 g = GlobalVariables()
 
